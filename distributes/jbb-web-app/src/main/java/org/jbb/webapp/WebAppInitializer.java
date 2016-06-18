870d09d0e2c63f71e3d632dd2cdcf3db9519b4fa
Bartek Ł <baart92@gmail.com>
2016-06-18 19:36:44 +0200

    Empty web application

---
+++ distributes/jbb-web-app/src/main/java/org/jbb/webapp/WebAppInitializer.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2016 the original author or authors.
+ *
+ * This file is part of jBB Application Project.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  You may obtain a copy of the License at
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package org.jbb.webapp;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext mvcContext = new AnnotationConfigWebApplicationContext();
+        ServletRegistration.Dynamic appServlet = servletContext.addServlet("jbbWebAppServlet", new DispatcherServlet(mvcContext));
+        appServlet.setLoadOnStartup(1);
+        appServlet.addMapping("/");
+    }
+}
