40cc3dae757285f53c2033d872ded03ae7c89898
Piotr <badzio92@gmail.com>
2016-09-01 23:31:17 +0200

    Stable version of default error controller. For now without recognize HTTP Status Code. All erros are catching by this Handler

--- implementation/domain/logic/jbb-frontend-web/src/main/java/org/jbb/frontend/web/handler/DefaultRequestExceptionHandler.java
+++ implementation/domain/logic/jbb-frontend-web/src/main/java/org/jbb/frontend/web/handler/DefaultRequestExceptionHandler.java
@@ -10,8 +10,6 @@
 
 package org.jbb.frontend.web.handler;
 
-import org.jbb.frontend.web.interceptors.BoardNameInterceptor;
-import org.jbb.frontend.web.interceptors.JbbVersionInterceptor;
 import org.jbb.frontend.web.interceptors.ReplacingViewInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -30,26 +28,17 @@ public class DefaultRequestExceptionHandler {
     @Autowired
     private ReplacingViewInterceptor replacingViewInterceptor;
 
-    @Autowired
-    private JbbVersionInterceptor jbbVersionInterceptor;
-
-    @Autowired
-    private BoardNameInterceptor boardNameInterceptor;
-
-
-    //    @ResponseStatus(value = HttpStatus.NO_CONTENT,reason = "Bo tak!")
     @ExceptionHandler(value = {RuntimeException.class, Exception.class, NoHandlerFoundException.class})
-//    @ResponseBody
     public ModelAndView defaultErrorHandler(HttpServletResponse response, HttpServletRequest request, Exception e) throws Exception {
-//        return DEFAULT_ERROR_VIEW_NAME; //new ErrorInfo(request.getRequestURL().toString(), e);
         ModelAndView modelAndView = new ModelAndView(DEFAULT_ERROR_VIEW_NAME);
+
         modelAndView.addObject("requestURL", request.getRequestURL());
         modelAndView.addObject("message", e.getMessage());
         modelAndView.addObject("status", response.getStatus());
+        modelAndView.addObject("jbbVersion", request.getAttribute("jbbVersion"));
+        modelAndView.addObject("boardName", request.getAttribute("boardName"));
 
-//        replacingViewInterceptor.postHandle(request, response, new Object(), modelAndView);
-//        jbbVersionInterceptor.postHandle(request, response, new Object(), modelAndView);
-//        boardNameInterceptor.postHandle(request, response, new Object(), modelAndView);
+        replacingViewInterceptor.postHandle(request, response, new Object(), modelAndView);
 
         return modelAndView;
     }
