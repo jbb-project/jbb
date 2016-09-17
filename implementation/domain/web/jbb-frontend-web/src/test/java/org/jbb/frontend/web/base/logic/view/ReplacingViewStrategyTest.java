eaf949634f29c864f2bbc2bb1e4695442efbd1d0
Bartek Ł <baart92@gmail.com>
2016-09-17 23:22:57 +0200

    Minor fixes in templates

---
+++ implementation/domain/web/jbb-frontend-web/src/test/java/org/jbb/frontend/web/base/logic/view/ReplacingViewStrategyTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2016 the original author or authors.
+ *
+ * This file is part of jBB Application Project.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  You may obtain a copy of the License at
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package org.jbb.frontend.web.base.logic.view;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ReplacingViewStrategyTest {
+    @Spy
+    private ReplacingViewStrategy replacingViewStrategy;
+
+    @Test
+    public void shouldPerformHandle_whenCan() throws Exception {
+        // given
+        when(replacingViewStrategy.canHandle(any())).thenReturn(true);
+
+        // when
+        replacingViewStrategy.handle(mock(ModelAndView.class));
+
+        // then
+        verify(replacingViewStrategy).performHandle(any());
+    }
+
+    @Test
+    public void shouldNotPerformHandle_whenCanNot() throws Exception {
+        // given
+        when(replacingViewStrategy.canHandle(any())).thenReturn(false);
+
+        // when
+        replacingViewStrategy.handle(mock(ModelAndView.class));
+
+        // then
+        verify(replacingViewStrategy, times(0)).performHandle(any());
+    }
+}
