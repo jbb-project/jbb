050e1bd0201ef9d782b2f7aaf3b8c89c7a913378
Bartek Ł <baart92@gmail.com>
2016-09-24 13:40:01 +0200

    Statistics in ucp

--- implementation/domain/web/jbb-members-web/src/main/java/org/jbb/members/web/base/controller/UcpStatisticsController.java
+++ implementation/domain/web/jbb-members-web/src/main/java/org/jbb/members/web/base/controller/UcpStatisticsController.java
@@ -10,13 +10,29 @@
 
 package org.jbb.members.web.base.controller;
 
+import org.jbb.lib.core.vo.Username;
+import org.jbb.members.api.data.RegistrationMetaData;
+import org.jbb.members.api.service.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UcpStatisticsController {
+    @Autowired
+    private RegistrationService registrationService;
+
     @RequestMapping(value = "/ucp/overview/statistics")
-    public String statistics() {
+    public String statistics(Model model, Authentication authentication) {
+        User currentUser = (User) authentication.getPrincipal();
+
+        RegistrationMetaData registrationMetaData = registrationService.getRegistrationMetaData(
+                Username.builder().value(currentUser.getUsername()).build());
+
+        model.addAttribute("joinTime", registrationMetaData.getJoinDateTime());
         return "ucp/overview/statistics";
     }
 }
