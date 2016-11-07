9515c7efe4adef2b06a287fd96d646bfa3cd67c8
Bartek Ł <baart92@gmail.com>
2016-11-07 22:48:23 +0100

    Member search works

--- implementation/domain/web/jbb-members-web/src/main/java/org/jbb/members/web/base/controller/AcpManageMemberController.java
+++ implementation/domain/web/jbb-members-web/src/main/java/org/jbb/members/web/base/controller/AcpManageMemberController.java
@@ -10,14 +10,22 @@
 
 package org.jbb.members.web.base.controller;
 
+import org.jbb.members.api.data.MemberSearchCriteria;
 import org.jbb.members.api.service.MemberService;
+import org.jbb.members.web.base.data.MemberSearchRow;
 import org.jbb.members.web.base.form.SearchMemberForm;
+import org.jbb.members.web.base.logic.MemberSearchCriteriaFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/acp/members/manage")
@@ -27,15 +35,38 @@ public class AcpManageMemberController {
     private static final String SEARCH_FORM_SENT_FLAG = "memberSearchFormSent";
 
     private final MemberService memberService;
+    private final MemberSearchCriteriaFactory criteriaFactory;
 
     @Autowired
-    public AcpManageMemberController(MemberService memberService) {
+    public AcpManageMemberController(MemberService memberService,
+                                     MemberSearchCriteriaFactory criteriaFactory) {
         this.memberService = memberService;
+        this.criteriaFactory = criteriaFactory;
     }
 
     @RequestMapping(method = RequestMethod.GET)
     public String membersSearchGet(Model model,
                                    @ModelAttribute(MEMBERS_SEARCH_FORM) SearchMemberForm form) {
+        return VIEW_NAME;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String membersSearchPost(Model model,
+                                    @ModelAttribute(MEMBERS_SEARCH_FORM) SearchMemberForm form,
+                                    BindingResult bindingResult,
+                                    RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute(SEARCH_FORM_SENT_FLAG, false);
+            return VIEW_NAME;
+        }
+        MemberSearchCriteria criteria = criteriaFactory.build(form);
+        List<MemberSearchRow> result = memberService.getAllMembersWithCriteria(criteria).stream()
+                .map(member ->
+                        new MemberSearchRow(member.getUsername(), member.getDisplayedName(),
+                                member.getEmail(), member.getRegistrationMetaData().getJoinDateTime()))
+                .collect(Collectors.toList());
+        model.addAttribute(SEARCH_FORM_SENT_FLAG, true);
+        model.addAttribute("memberRows", result);
         return VIEW_NAME;
     }
 }
