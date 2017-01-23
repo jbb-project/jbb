b27ec4ab6d032d68b3b4ce63b86e9ee24fc8f70c
Bartek Ł <baart92@gmail.com>
2017-01-23 02:22:45 +0100

    Edit appender hyperlink works

--- implementation/domain/web/jbb-system-web/src/main/java/org/jbb/system/web/logging/controller/AcpAppenderController.java
+++ implementation/domain/web/jbb-system-web/src/main/java/org/jbb/system/web/logging/controller/AcpAppenderController.java
@@ -11,6 +11,8 @@
 package org.jbb.system.web.logging.controller;
 
 import org.jbb.system.api.model.logging.LogAppender;
+import org.jbb.system.api.model.logging.LogConsoleAppender;
+import org.jbb.system.api.model.logging.LogFileAppender;
 import org.jbb.system.api.service.LoggingSettingsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,7 +26,8 @@ import java.util.Optional;
 @Controller
 @RequestMapping("/acp/general/logging/append")
 public class AcpAppenderController {
-    private static final String VIEW_NAME = "acp/general/append";
+    private static final String CONSOLE_APPENDER_VIEW_NAME = "acp/general/appender-console";
+    private static final String FILE_APPENDER_VIEW_NAME = "acp/general/appender-file";
     private static final String APPENDER_FORM = "appenderForm";
     private static final String NEW_APPENDER_STATE = "newAppenderState";
     private static final String FORM_SAVED_FLAG = "appenderFormSaved";
@@ -40,19 +43,34 @@ public class AcpAppenderController {
     public String loggerGet(@RequestParam(value = "act") String action,
                             @RequestParam(value = "id", required = false) String appenderName,
                             Model model) {
+        if ("newconsole".equals(action)) {
+            return CONSOLE_APPENDER_VIEW_NAME;
+        } else if ("newfile".equals(action)) {
+            return FILE_APPENDER_VIEW_NAME;
+        }
+
         Optional<LogAppender> appender = loggingSettingsService.getAppender(appenderName);
         if (!appender.isPresent()) {
             throw new IllegalStateException("Appender with name '" + appenderName + "' doesn't exist");
         }
 
         if ("edit".equals(action)) {
-            //todo
-            return "redirect:/acp/general/logging";
+            //todo - insert appender to model
+            return resolveView(appender.get());
         } else if ("del".equals(action)) {
             loggingSettingsService.deleteAppender(appender.get());
             return "redirect:/acp/general/logging";
         } else {
             throw new IllegalStateException("Incorrect action: " + action);
         }
+    }
+
+    private String resolveView(LogAppender appender) {
+        if (appender instanceof LogConsoleAppender) {
+            return CONSOLE_APPENDER_VIEW_NAME;
+        } else if (appender instanceof LogFileAppender) {
+            return FILE_APPENDER_VIEW_NAME;
+        }
+        throw new IllegalArgumentException("Unknown view for appender: " + appender);
     }
 }
