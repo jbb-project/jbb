e3456af6743ae1142a697a595c41d77df776f304
Piotr <badzio92@gmail.com>
2017-01-27 00:01:19 +0100

    Almost completed UL main UI Added completed flow with UI -> Controller -> UI

--- implementation/domain/web/jbb-security-web/src/main/java/org/jbb/security/web/acp/controller/AcpUserLockController.java
+++ implementation/domain/web/jbb-security-web/src/main/java/org/jbb/security/web/acp/controller/AcpUserLockController.java
@@ -11,16 +11,19 @@
 package org.jbb.security.web.acp.controller;
 
 import com.google.common.collect.Maps;
+
 import org.jbb.security.api.model.UserLockSettings;
 import org.jbb.security.api.service.UserLockService;
+import org.jbb.security.web.acp.form.UserLockServiceSettingsForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,45 +32,67 @@ import java.util.Map;
 public class AcpUserLockController {
 
     private static final String VIEW_NAME = "acp/general/lock";
+    private static final String SETTING_FORM = "settings";
+
+    private final static String MAXIMUM_INVALID_ATTEMPTS = "Maximum of invalid attempts: ";
+    private final static String SERVICE_AVAILABLE = "Does User Lock Service is available: ";
+    private final static String MEASUREMENT_TIME_PERIOD = "Time period where attempts are measured: ";
+    private final static String ACCOUNT_LOCK_TIME_PERIOD = "How long is account lock: ";
 
     @Autowired
     private UserLockService userLockService;
 
 
     @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView userLockSettingsPanelGet() {
-        ModelAndView modelAndView = new ModelAndView(VIEW_NAME);
-        modelAndView.addObject("data", getData());
-
-        return modelAndView;
+    public String userLockSettingsPanelGet(Model model, @ModelAttribute(SETTING_FORM) UserLockServiceSettingsForm settings) {
+        model.addAttribute("data", getData());
+        model.addAttribute(SETTING_FORM, new UserLockServiceSettingsForm());
+        return VIEW_NAME;
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String userLockSettingsPanelPost(@Valid UserLockSettings settings, BindingResult bindingResult) {
-        if( bindingResult.hasErrors()){
+    public String userLockSettingsPanelPost(@ModelAttribute("settings") UserLockServiceSettingsForm settings, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
             return VIEW_NAME;
         }
-        userLockService.setProperties(settings);
+        UserLockSettings serviceSettings = createSettings(settings);
+        userLockService.setProperties(serviceSettings);
 
-        return "redirect:/"+VIEW_NAME;
+        return "redirect:/" + VIEW_NAME;
     }
 
     private Map<String, String> getData() {
         HashMap<String, String> data = Maps.newHashMap();
-        data.put(UserLockViewColumns.MAXIMUM_INVALID_ATTEMPTS, String.valueOf(userLockService.getUserLockServiceSettings().maximumNumberOfInvalidSignInAttempts()));
-        data.put(UserLockViewColumns.SERVICE_AVAILABLE, Boolean.toString(userLockService.getUserLockServiceSettings().serviceAvailable()));
-        data.put(UserLockViewColumns.MEASUREMENT_TIME_PERIOD, String.valueOf(userLockService.getUserLockServiceSettings().invalidAttemptsMeasurementTimePeriod()));
-        data.put(UserLockViewColumns.ACCOUNT_LOCK_TIME_PERIOD, String.valueOf(userLockService.getUserLockServiceSettings().accountLockTimePeriod()));
+        data.put(MAXIMUM_INVALID_ATTEMPTS, String.valueOf(userLockService.getUserLockServiceSettings().maximumNumberOfInvalidSignInAttempts()));
+        data.put(SERVICE_AVAILABLE, Boolean.toString(userLockService.getUserLockServiceSettings().serviceAvailable()));
+        data.put(MEASUREMENT_TIME_PERIOD, String.valueOf(userLockService.getUserLockServiceSettings().invalidAttemptsMeasurementTimePeriod()));
+        data.put(ACCOUNT_LOCK_TIME_PERIOD, String.valueOf(userLockService.getUserLockServiceSettings().accountLockTimePeriod()));
 
         return data;
     }
 
-    private final class UserLockViewColumns {
+    private UserLockSettings createSettings(UserLockServiceSettingsForm settings) {
+        return new UserLockSettings() {
+            @Override
+            public int maximumNumberOfInvalidSignInAttempts() {
+                return settings.getMaximumNumberOfInvalidSignInAttempts();
+            }
 
-        public final static String MAXIMUM_INVALID_ATTEMPTS = "Maximum of invalid attempts: ";
-        public final static String SERVICE_AVAILABLE = "Does User Lock Service is available: ";
-        public final static String MEASUREMENT_TIME_PERIOD = "Time period where attempts are measured: ";
-        public final static String ACCOUNT_LOCK_TIME_PERIOD = "How long is account lock: ";
+            @Override
+            public Long invalidAttemptsMeasurementTimePeriod() {
+                return settings.getInvalidAttemptsMeasurementTimePeriod();
+            }
 
+            @Override
+            public Long accountLockTimePeriod() {
+                return settings.getAccountLockTimePeriod();
+            }
+
+            @Override
+            public boolean serviceAvailable() {
+                return settings.isServiceAvailable();
+            }
+        };
     }
+
 }
