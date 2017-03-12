f0611bea3b6c1779b7300b120dfdf3fed27df5a4
Bartek Ł <baart92@gmail.com>
2017-03-12 21:51:44 +0100

    Error on demand controller (for E2E testing stacktrace visibility feature)

---
+++ implementation/domain/web/jbb-system-web/src/main/java/org/jbb/system/web/logging/controller/ErrorOnDemandController.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2017 the original author or authors.
+ *
+ * This file is part of jBB Application Project.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  You may obtain a copy of the License at
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package org.jbb.system.web.logging.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/err")
+public class ErrorOnDemandController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String exampleErrorGet() {
+        throw new IllegalStateException("Bad request");
+    }
+}
