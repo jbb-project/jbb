5904a71f8351a0665ce7de15b368136a7825d3f1
Piotr <badzio92@gmail.com>
2017-04-18 22:58:47 +0200

    Change way how session event service is catching session event. Add all type of event in one service - but separate method

--- implementation/domain/logic/jbb-system-impl/src/main/java/org/jbb/system/impl/sesssion/event/SessionEventService.java
+++ implementation/domain/logic/jbb-system-impl/src/main/java/org/jbb/system/impl/sesssion/event/SessionEventService.java
@@ -1,25 +1,43 @@
 package org.jbb.system.impl.sesssion.event;
 
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.core.session.SessionDestroyedEvent;
 import org.springframework.session.MapSession;
 import org.springframework.session.events.SessionCreatedEvent;
+import org.springframework.session.events.SessionDeletedEvent;
+import org.springframework.session.events.SessionExpiredEvent;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
-public class SessionEventService implements ApplicationListener<SessionCreatedEvent> {
+public class SessionEventService {
 
     private Map<String,MapSession> sessionMap;
 
     public SessionEventService(){
-        this.sessionMap = new HashMap<>();
+        this.sessionMap = new ConcurrentHashMap<>();
     }
 
-    @Override
-    public void onApplicationEvent(SessionCreatedEvent sessionCreatedEvent) {
+    @EventListener
+    public void onSessionCreatedEvent(SessionCreatedEvent sessionCreatedEvent) {
         this.sessionMap.put(sessionCreatedEvent.getSessionId(),sessionCreatedEvent.getSession());
+    }
+
+    @EventListener
+    public void onSessionDeletedEvent(SessionDeletedEvent sessionDeletedEvent){
+        this.sessionMap.remove(sessionDeletedEvent.getSessionId());
+    }
+
+    @EventListener
+    public void onSessionDestroyedEvent(SessionDestroyedEvent sessionDestroyedEvent){
+        this.sessionMap.remove(sessionDestroyedEvent.getId());
+    }
+
+    @EventListener
+    public void onSessionExpiredEvent(SessionExpiredEvent sessionExpiredEvent){
+        this.sessionMap.remove(sessionExpiredEvent.getSessionId());
     }
 
     public Map<String,MapSession> getSessionMap(){
