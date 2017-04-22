38bca8b6df1ab968c418c7435190729f82c97c7d
Piotr <badzio92@gmail.com>
2017-04-22 22:45:19 +0200

    Add @Repository stereotype

--- implementation/libs/jbb-lib-mvc/src/main/java/org/jbb/lib/mvc/repository/JbbSessionRepository.java
+++ implementation/libs/jbb-lib-mvc/src/main/java/org/jbb/lib/mvc/repository/JbbSessionRepository.java
@@ -4,6 +4,7 @@ package org.jbb.lib.mvc.repository;
 import org.springframework.session.ExpiringSession;
 import org.springframework.session.MapSession;
 import org.springframework.session.MapSessionRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -14,6 +15,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * Method is using in administrator panel in session bookmark
  *
  */
+@Repository
 public class JbbSessionRepository extends MapSessionRepository{
 
     private Integer defaultMaxInactiveInterval;
