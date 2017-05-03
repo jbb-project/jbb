c42e5bc932f33c1a30128ac74dfe6e6a844e7d31
Bartek Ł <baart92@gmail.com>
2017-05-03 16:29:30 +0200

    show forums at home page

--- implementation/domain/web/jbb-frontend-web/src/main/java/org/jbb/frontend/web/base/controller/HomePageController.java
+++ implementation/domain/web/jbb-frontend-web/src/main/java/org/jbb/frontend/web/base/controller/HomePageController.java
@@ -10,13 +10,23 @@
 
 package org.jbb.frontend.web.base.controller;
 
+import org.jbb.board.api.model.Forum;
+import org.jbb.board.api.model.ForumCategory;
 import org.jbb.board.api.service.BoardService;
+import org.jbb.frontend.web.base.data.ForumCategoryRow;
+import org.jbb.frontend.web.base.data.ForumRow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomePageController {
+    private static final String HOME_VIEW_NAME = "home";
+
     private final BoardService boardService;
 
     @Autowired
@@ -25,8 +35,37 @@ public class HomePageController {
     }
 
     @RequestMapping("/")
-    public String main() {
-        return "home"; //NOSONAR
+    public String main(Model model) {
+        List<ForumCategory> forumCategories = boardService.getForumCategories();
+
+        List<ForumCategoryRow> forumStructureRows = forumCategories.stream()
+                .map(forumCategory -> createDto(forumCategory))
+                .collect(Collectors.toList());
+
+        model.addAttribute("forumStructure", forumStructureRows);
+
+        return HOME_VIEW_NAME;
+    }
+
+    private ForumCategoryRow createDto(ForumCategory forumCategory) {
+        ForumCategoryRow categoryRow = new ForumCategoryRow();
+        categoryRow.setName(forumCategory.getName());
+
+        List<ForumRow> forumRows = forumCategory.getForums().stream()
+                .map(forum -> createDto(forum))
+                .collect(Collectors.toList());
+
+        categoryRow.setForumRows(forumRows);
+        return categoryRow;
+    }
+
+    private ForumRow createDto(Forum forum) {
+        return ForumRow.builder()
+                .id(forum.getId())
+                .name(forum.getName())
+                .description(forum.getDescription())
+                .locked(forum.isLocked())
+                .build();
     }
 
     @RequestMapping("/faq")
