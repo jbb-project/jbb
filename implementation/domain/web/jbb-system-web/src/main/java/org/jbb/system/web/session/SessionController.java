89132ac5c5679e2bca989a6d6648166be50603ed
Bartek Ł <baart92@gmail.com>
2017-05-18 00:12:13 +0200

    session deletion though acp session management

--- implementation/domain/web/jbb-system-web/src/main/java/org/jbb/system/web/session/SessionController.java
+++ implementation/domain/web/jbb-system-web/src/main/java/org/jbb/system/web/session/SessionController.java
@@ -1,21 +1,33 @@
+/*
+ * Copyright (C) 2017 the original author or authors.
+ *
+ * This file is part of jBB Application Project.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  You may obtain a copy of the License at
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package org.jbb.system.web.session;
 
 import org.jbb.system.api.model.session.UserSession;
 import org.jbb.system.api.service.SessionService;
 import org.jbb.system.web.session.data.SessionUITableRow;
 import org.jbb.system.web.session.form.InactiveIntervalTimeForm;
+import org.jbb.system.web.session.form.SessionRemoveForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -25,14 +37,12 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/acp/system/sessions")
 public class SessionController {
 
-    private SessionService sessionService;
-
     private static final String VIEW_NAME = "acp/system/sessions";
     private static final String USER_SESSION_MODEL_ATTRIBUTE = "userSessions";
     private static final String SESSION_FORM_NAME = "sessionSettingForm";
     private static final String SESSION_REMOVE_FORM_NAME = "sessionRemoveForm";
-
     private static final String FORM_SAVED_CORRECT_STATUS_FLAG = "sessionFormSaved";
+    private SessionService sessionService;
 
     @Autowired
     public SessionController(SessionService sessionService){
@@ -83,11 +93,10 @@ public class SessionController {
         }
     }
 
-    @RequestMapping(value = "/removesession",method = RequestMethod.POST)
-    public String removeSession(@RequestParam("sessionID") String sessionID){
+    @RequestMapping(value = "/remove", method = RequestMethod.POST)
+    public String removeSession(@ModelAttribute(SESSION_REMOVE_FORM_NAME) @Valid SessionRemoveForm form) {
 
-        System.out.println("Abc");
-        sessionService.terminateSession(sessionID);
+        sessionService.terminateSession(form.getId());
         return "redirect:/"+VIEW_NAME;
     }
 }
