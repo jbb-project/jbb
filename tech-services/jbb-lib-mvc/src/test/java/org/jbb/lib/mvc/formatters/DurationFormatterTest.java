668887683eb10146351757d0d23b937df27a6f95
Bartek Ł <baart92@gmail.com>
2017-06-03 20:38:10 +0200

    move maven modules

--- tech-services/jbb-lib-mvc/src/test/java/org/jbb/lib/mvc/formatters/DurationFormatterTest.java
+++ tech-services/jbb-lib-mvc/src/test/java/org/jbb/lib/mvc/formatters/DurationFormatterTest.java
@@ -13,7 +13,9 @@ import java.util.Locale;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DurationFormatterTest {
