0f6caae0431c7661b013268de3197c06eea703b8
Bartek Ł <baart92@gmail.com>
2017-06-04 22:53:13 +0200

    commit missing

--- domain-api/jbb-system-api/src/main/java/org/jbb/system/api/session/SessionService.java
+++ domain-api/jbb-system-api/src/main/java/org/jbb/system/api/session/SessionService.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 public interface SessionService {
 
-    List<UserSession> getAllUserSessions();
+    List<MemberSession> getAllUserSessions();
 
     void terminateSession(String sessionId);
 
