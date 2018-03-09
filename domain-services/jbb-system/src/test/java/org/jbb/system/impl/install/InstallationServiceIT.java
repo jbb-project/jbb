ea78432019e40af3db2e18864b3684c6b7a6977e
Bartek Ł <baart92@gmail.com>
2018-03-09 21:01:29 +0100

    Installation service tests

--- domain-services/jbb-system/src/test/java/org/jbb/system/impl/install/InstallationServiceIT.java
+++ domain-services/jbb-system/src/test/java/org/jbb/system/impl/install/InstallationServiceIT.java
@@ -10,13 +10,48 @@
 
 package org.jbb.system.impl.install;
 
+import org.jbb.install.InstallationData;
+import org.jbb.install.database.DatabaseInstallationData;
+import org.jbb.install.database.DatabaseType;
+import org.jbb.system.api.install.InstallationService;
 import org.jbb.system.impl.BaseIT;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class InstallationServiceIT extends BaseIT {
 
-    @Test
-    public void name() {
+    @Autowired
+    private InstallationService installationService;
 
+    @Test
+    public void shouldInstallSuccessful() {
+        // given
+        InstallationData data = validInstallationData();
+        assertThat(installationService.isInstalled()).isFalse();
+        assertThat(installationService.getInstalledSteps()).isEmpty();
+
+        // when
+        installationService.install(data);
+
+        // then
+        assertThat(installationService.isInstalled()).isTrue();
+        assertThat(installationService.getInstalledSteps()).isNotEmpty();
+    }
+
+    private InstallationData validInstallationData() {
+        return InstallationData.builder()
+                .adminUsername("admin")
+                .adminDisplayedName("Admin")
+                .adminEmail("admin@admin.com")
+                .adminPassword("admin")
+                .boardName("jBB Testing")
+                .databaseInstallationData(
+                        DatabaseInstallationData.builder()
+                                .databaseType(DatabaseType.H2_IN_MEMORY)
+                                .build()
+                )
+                .build();
     }
 }
