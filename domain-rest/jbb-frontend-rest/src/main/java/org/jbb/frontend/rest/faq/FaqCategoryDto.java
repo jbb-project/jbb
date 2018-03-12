6d5668e23fb862fc5c50ac92c4b1c09e338f9a43
Bartek Ł <baart92@gmail.com>
2018-03-12 12:20:35 +0100

    Faq category not null

--- domain-rest/jbb-frontend-rest/src/main/java/org/jbb/frontend/rest/faq/FaqCategoryDto.java
+++ domain-rest/jbb-frontend-rest/src/main/java/org/jbb/frontend/rest/faq/FaqCategoryDto.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import io.swagger.annotations.ApiModel;
 import lombok.AccessLevel;
@@ -35,5 +36,6 @@ public class FaqCategoryDto {
     private String name;
 
     @Valid
+    @NotNull
     private List<FaqEntryDto> questions;
 }
