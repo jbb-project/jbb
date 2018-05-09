47d0e27c2eb2a4db2451ae22ff501eb60d595316
Bartek Ł <baart92@gmail.com>
2018-05-09 23:55:59 +0200

    Property files health check

--- tech-services/jbb-lib-properties/src/main/java/org/jbb/lib/properties/health/PropertiesFilesHealthCheck.java
+++ tech-services/jbb-lib-properties/src/main/java/org/jbb/lib/properties/health/PropertiesFilesHealthCheck.java
@@ -10,11 +10,45 @@
 
 package org.jbb.lib.properties.health;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.jbb.lib.health.JbbHealthCheck;
+import org.jbb.lib.properties.JbbPropertyFilesResolver;
+import org.jbb.lib.properties.ModuleStaticProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class PropertiesFilesHealthCheck extends JbbHealthCheck {
+
+    private final JbbPropertyFilesResolver jbbPropertyFilesResolver;
+    private final ApplicationContext applicationContext;
+
+    private Map<ModuleStaticProperties, File> filesMonitored;
+
+    @PostConstruct
+    public void fillMonitoredFiles() {
+        List<ModuleStaticProperties> modulePropertiesBeans = new ArrayList<>(
+            applicationContext.getBeansOfType(ModuleStaticProperties.class).values());
+        filesMonitored = modulePropertiesBeans.stream()
+            .collect(Collectors.toMap(Function.identity(), this::getFile));
+    }
+
+    private File getFile(ModuleStaticProperties moduleProperties) {
+        return jbbPropertyFilesResolver.resolvePropertyFileNames(
+            (Class<? extends ModuleStaticProperties>) moduleProperties.getClass()
+                .getInterfaces()[0])
+            .stream()
+            .map(File::new).limit(1).collect(Collectors.toList()).get(0);
+    }
 
     @Override
     public String getName() {
@@ -23,6 +57,32 @@ public class PropertiesFilesHealthCheck extends JbbHealthCheck {
 
     @Override
     protected Result check() throws Exception {
-        return Result.healthy();//fixme
+        ResultBuilder builder = Result.builder();
+        boolean healthyFlag = true;
+        for (Entry<ModuleStaticProperties, File> moduleProperties : filesMonitored.entrySet()) {
+            String modulePropertiesName = moduleProperties.getKey().getClass().getInterfaces()[0]
+                .getCanonicalName();
+            File propertyFile = moduleProperties.getValue();
+
+            String message = null;
+            if (!propertyFile.exists()) {
+                message = "File %s does not exist";
+            } else if (propertyFile.isDirectory()) {
+                message = "File %s is a directory";
+            } else if (!propertyFile.canRead()) {
+                message = "File %s is not readable";
+            } else if (!propertyFile.canWrite()) {
+                message = "File %s is not writable";
+            }
+
+            if (message != null) {
+                healthyFlag = false;
+            } else {
+                message = "File %s is OK";
+            }
+            builder.withDetail(modulePropertiesName,
+                String.format(message, propertyFile.getAbsolutePath()));
+        }
+        return healthyFlag ? builder.healthy().build() : builder.unhealthy().build();
     }
 }
