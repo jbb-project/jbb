40e563e4ad6b6333ffa3444102cf3590af7c0678
Bartek Ł <baart92@gmail.com>
2018-05-27 22:37:07 +0200

    Basic and Bearer autorization method

--- domain-web/jbb-security-web/src/main/java/org/jbb/security/web/Oauth2ResourceServerConfig.java
+++ domain-web/jbb-security-web/src/main/java/org/jbb/security/web/Oauth2ResourceServerConfig.java
@@ -14,6 +14,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableResourceServer
@@ -27,9 +30,19 @@ public class Oauth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.requestMatchers().antMatchers("/api/**")
-                .and().authorizeRequests().anyRequest().permitAll();
+        http.requestMatcher(new OAuthRequestedMatcher())
+                .authorizeRequests()
+                .anyRequest().authenticated();
 //        http.oauth2Login().successHandler(rootAuthSuccessHandler);
 //        http.oauth2Login().failureHandler(rootAuthFailureHandler);
+    }
+
+    private static class OAuthRequestedMatcher implements RequestMatcher {
+        @Override
+        public boolean matches(HttpServletRequest request) {
+            String auth = request.getHeader("Authorization");
+            // Determine if the client request contained an OAuth Authorization
+            return (auth != null) && auth.startsWith("Bearer");
+        }
     }
 }
