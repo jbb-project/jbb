d01b0d9d30ee0301bc591b521ba04671cffbef09
Bartek Ł <baart92@gmail.com>
2018-06-01 00:17:05 +0200

    Fix MockSppringSecurityConfig

--- tech-services/jbb-lib-test/src/main/java/org/jbb/lib/test/MockSpringSecurityConfig.java
+++ tech-services/jbb-lib-test/src/main/java/org/jbb/lib/test/MockSpringSecurityConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 the original author or authors.
+ * Copyright (C) 2018 the original author or authors.
  *
  * This file is part of jBB Application Project.
  *
@@ -12,33 +12,46 @@ package org.jbb.lib.test;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
-public class MockSpringSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Bean
-    public TestbedSecurityContextRepository testbedSecurityContextRepository() {
-        return new TestbedSecurityContextRepository();
-    }
+public class MockSpringSecurityConfig extends GlobalMethodSecurityConfiguration {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
-        http.securityContext().securityContextRepository(testbedSecurityContextRepository());
-        http.csrf().disable();
+    protected MethodSecurityExpressionHandler createExpressionHandler() {
+        return new OAuth2MethodSecurityExpressionHandler();
     }
 
-    @Bean
-    public SessionRegistry sessionRegistry() {
-        return new SessionRegistryImpl();
+    @Configuration
+    public class SecurityAdapterConfig extends WebSecurityConfigurerAdapter {
+
+        @Bean
+        public TestbedSecurityContextRepository testbedSecurityContextRepository() {
+            return new TestbedSecurityContextRepository();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll();
+            http.securityContext().securityContextRepository(testbedSecurityContextRepository());
+            http.csrf().disable();
+        }
+
+        @Bean
+        public SessionRegistry sessionRegistry() {
+            return new SessionRegistryImpl();
+        }
+
     }
 
 }
