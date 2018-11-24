26278bf05c3ae03c0fc4098e15111cdc9b74e02d
Bartek Ł <baart92@gmail.com>
2018-11-24 12:01:52 +0100

    Draft of ACP view for OAuth client browser

---
+++ domain-web/jbb-security-web/src/main/java/org/jbb/security/web/signin/SignInUrlAuthFailureHandler.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2018 the original author or authors.
+ *
+ * This file is part of jBB Application Project.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  You may obtain a copy of the License at
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package org.jbb.security.web.signin;
+
+import org.jbb.lib.commons.vo.Username;
+import org.jbb.lib.eventbus.JbbEventBus;
+import org.jbb.members.api.base.Member;
+import org.jbb.members.api.base.MemberService;
+import org.jbb.security.api.lockout.MemberLockoutService;
+import org.jbb.security.event.SignInFailedEvent;
+import org.jbb.security.web.SecurityWebConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class SignInUrlAuthFailureHandler extends SimpleUrlAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    private final MemberService memberService;
+    private final JbbEventBus eventBus;
+    private final MemberLockoutService memberLockoutService;
+
+    @Autowired
+    public SignInUrlAuthFailureHandler(MemberService memberService, JbbEventBus eventBus, MemberLockoutService memberLockoutService) {
+        super(SecurityWebConfig.LOGIN_FAILURE_URL);
+        this.memberService = memberService;
+        this.memberLockoutService = memberLockoutService;
+        this.eventBus = eventBus;
+    }
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException e)
+            throws IOException, ServletException {
+        Username username = Username.builder().value(request.getParameter("username")).build();
+        Long memberId = tryToResolveMemberId(username);
+        log.debug("Sign in attempt failure for member with username '{}' (member id: {})", username.getValue(), memberId);
+        memberLockoutService.lockMemberIfQualify(memberId);
+        super.onAuthenticationFailure(request, response, e);
+        eventBus.post(new SignInFailedEvent(memberId, username));
+    }
+
+    private Long tryToResolveMemberId(Username username) {
+        Optional<Member> memberWithUsername = memberService.getMemberWithUsername(username);
+        return memberWithUsername.map(Member::getId).orElse(null);
+    }
+}
---
+++ domain-web/jbb-security-web/src/test/java/org/jbb/security/web/signin/SignInControllerTest.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2018 the original author or authors.
+ *
+ * This file is part of jBB Application Project.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  You may obtain a copy of the License at
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package org.jbb.security.web.signin;
+
+import org.jbb.lib.mvc.flow.RedirectManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.ui.Model;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SignInControllerTest {
+    @Mock
+    private RedirectManager redirectManagerMock;
+
+    @InjectMocks
+    private SignInController signInController;
+
+    @Test
+    public void shouldGoToPreviousPage_whenAuthenticatedUserOpenedSignInPage() {
+        // given
+        Model modelMock = mock(Model.class);
+        HttpServletRequest httpServletRequestMock = mock(HttpServletRequest.class);
+        Authentication authenticationMock = mock(Authentication.class); // user authenticated
+        given(authenticationMock.isAuthenticated()).willReturn(true);
+        HttpSession httpSessionMock = mock(HttpSession.class);
+
+        // when
+        signInController.signIn(null, modelMock, httpServletRequestMock, authenticationMock, httpSessionMock);
+
+        // then
+        verify(redirectManagerMock, times(1)).goToPreviousPageSafe(eq(httpServletRequestMock));
+    }
+
+    @Test
+    public void shouldToGoSignInView_whenAnonUserOpenedSignInPage() {
+        // given
+        Model modelMock = mock(Model.class);
+        HttpServletRequest httpServletRequestMock = mock(HttpServletRequest.class);
+        Authentication authenticationMock = mock(Authentication.class); // user not authenticated
+        given(authenticationMock.isAuthenticated()).willReturn(false);
+        HttpSession httpSessionMock = mock(HttpSession.class);
+
+        // when
+        String viewName = signInController.signIn(null, modelMock, httpServletRequestMock, authenticationMock, httpSessionMock);
+
+        // then
+        assertThat(viewName).isEqualTo("signin");
+    }
+
+    @Test
+    public void shouldAddErrorMessageAboutnInvalidUsernameOrPassword_whenErrorFlagPassed_andBadCredentialsExceptionHappened() {
+        // given
+        Model modelMock = mock(Model.class);
+        HttpServletRequest httpServletRequestMock = mock(HttpServletRequest.class);
+        HttpSession httpSessionMock = mock(HttpSession.class);
+        given(httpServletRequestMock.getSession()).willReturn(httpSessionMock);
+        given(httpSessionMock.getAttribute(eq("SPRING_SECURITY_LAST_EXCEPTION"))).willReturn(mock(BadCredentialsException.class));
+
+
+        Authentication authenticationMock = mock(Authentication.class); // user not authenticated
+        given(authenticationMock.isAuthenticated()).willReturn(false);
+
+        // when
+        String viewName = signInController.signIn(false, modelMock, httpServletRequestMock, authenticationMock, httpSessionMock);
+
+        // then
+        verify(modelMock, times(1)).addAttribute(eq("loginError"), eq("Invalid username or password"));
+    }
+
+    @Test
+    public void shouldAddGenericErrorMessage_whenErrorFlagPassed_andDifferentThanBadCredentialsExceptionHappened() {
+        // given
+        Model modelMock = mock(Model.class);
+        HttpServletRequest httpServletRequestMock = mock(HttpServletRequest.class);
+        HttpSession httpSessionMock = mock(HttpSession.class);
+        given(httpServletRequestMock.getSession()).willReturn(httpSessionMock);
+        given(httpSessionMock.getAttribute(eq("SPRING_SECURITY_LAST_EXCEPTION"))).willReturn(mock(IllegalStateException.class));
+
+
+        Authentication authenticationMock = mock(Authentication.class); // user not authenticated
+        given(authenticationMock.isAuthenticated()).willReturn(false);
+
+        // when
+        String viewName = signInController.signIn(false, modelMock, httpServletRequestMock, authenticationMock, httpSessionMock);
+
+        // then
+        verify(modelMock, times(1)).addAttribute(eq("loginError"), eq("Some error occurred. Please contact administrator"));
+    }
+
+}
