75c27b312e7708b943c2723b687ab218047fa8f3
Bartek Ł <baart92@gmail.com>
2018-12-27 01:32:39 +0100

    OAuth client management via ACP

--- domain-web/jbb-security-web/src/main/java/org/jbb/security/web/oauth/client/AcpOAuthClientController.java
+++ domain-web/jbb-security-web/src/main/java/org/jbb/security/web/oauth/client/AcpOAuthClientController.java
@@ -15,8 +15,10 @@ import com.google.common.collect.Lists;
 import org.apache.commons.lang3.StringUtils;
 import org.jbb.lib.commons.security.OAuthScope;
 import org.jbb.security.api.oauth.OAuthClient;
+import org.jbb.security.api.oauth.OAuthClientException;
 import org.jbb.security.api.oauth.OAuthClientNotFoundException;
 import org.jbb.security.api.oauth.OAuthClientsService;
+import org.jbb.security.api.oauth.SecretOAuthClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -27,26 +29,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 import static org.jbb.security.web.oauth.client.AcpOAuthClientsController.OAUTH_CLIENT_ACP_VIEW;
 
-@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/acp/system/oauth/clients")
 public class AcpOAuthClientController {
 
+    private static final String OAUTH_CLIENTS_URL = "acp/system/oauth/clients";
     private static final String OAUTH_CLIENT_DETAILS_ACP_VIEW = "acp/system/oauth-client";
     private static final String CLIENT_FORM = "clientForm";
     private static final String FORM_SAVED_FLAG = "clientFormSaved";
     private static final String NEW_CLIENT_STATE = "newClientState";
     private static final String SUPPORTED_SCOPES = "supportedScopes";
     private static final String CLIENT_ID_FORM = "clientIdForm";
+    private static final String SHOW_SECRET_FLAG = "showSecret";
 
     private final OAuthClientsService oAuthClientsService;
 
     private final OAuthClientFormTranslator formTranslator;
+    private final OAuthClientErrorsBindingMapper errorsBindingMapper;
 
     @RequestMapping(method = RequestMethod.GET)
     public String oauthClientGet(@RequestParam(value = "id", required = false) String clientId,
@@ -71,28 +74,45 @@ public class AcpOAuthClientController {
     public String oauthClientPost(@ModelAttribute(CLIENT_FORM) OAuthClientForm form, Model model,
                                   BindingResult bindingResult, RedirectAttributes redirectAttributes) throws OAuthClientNotFoundException {
         OAuthClient client = formTranslator.toClient(form);
-        if (form.isAddingMode()) {
-            oAuthClientsService.createClient(client);
-        } else {
-            oAuthClientsService.updateClient(form.getClientId(), client);
+        boolean addingMode = form.isAddingMode();
+        model.addAttribute(NEW_CLIENT_STATE, addingMode);
+        try {
+            if (addingMode) {
+                client = oAuthClientsService.createClient(client);
+                redirectAttributes.addFlashAttribute(SHOW_SECRET_FLAG, true);
+                redirectAttributes.addFlashAttribute("secret", ((SecretOAuthClient) client).getClientSecret());
+            } else {
+                client = oAuthClientsService.updateClient(form.getClientId(), client);
+            }
+            redirectAttributes.addFlashAttribute(FORM_SAVED_FLAG, true);
+        } catch (OAuthClientException ex) {
+            errorsBindingMapper.map(ex.getConstraintViolations(), bindingResult);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult." + CLIENT_FORM, bindingResult);
+            redirectAttributes.addFlashAttribute(CLIENT_FORM, form);
+            redirectAttributes.addFlashAttribute(FORM_SAVED_FLAG, false);
         }
-        redirectAttributes.addFlashAttribute(FORM_SAVED_FLAG, true);
-        return OAUTH_CLIENT_DETAILS_ACP_VIEW;
+
+        if (StringUtils.isNotBlank(form.getClientId())) {
+            redirectAttributes.addAttribute("id", client.getClientId());
+        }
+
+        return "redirect:/" + OAUTH_CLIENTS_URL;
     }
 
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    public String oauthClientDelete(@ModelAttribute(CLIENT_ID_FORM) OAuthClientIdForm form,
-                                    BindingResult bindingResult, RedirectAttributes redirectAttributes) throws OAuthClientNotFoundException {
+    public String oauthClientDelete(@ModelAttribute(CLIENT_ID_FORM) OAuthClientIdForm form) throws OAuthClientNotFoundException {
         oAuthClientsService.removeClient(form.getId());
         return "redirect:/" + OAUTH_CLIENT_ACP_VIEW;
     }
 
     @RequestMapping(value = "/regenerate", method = RequestMethod.POST)
     public String oauthClientRegenerate(@ModelAttribute(CLIENT_ID_FORM) OAuthClientIdForm form,
-                                        BindingResult bindingResult, RedirectAttributes redirectAttributes) throws OAuthClientNotFoundException {
+                                        RedirectAttributes redirectAttributes) throws OAuthClientNotFoundException {
         String newSecret = oAuthClientsService.generateClientSecret(form.getId());
-        log.info("Client '{}' nas new secret: {}", form.getId(), newSecret);
-        return "redirect:/acp/system/oauth/clients";
+        redirectAttributes.addFlashAttribute(SHOW_SECRET_FLAG, true);
+        redirectAttributes.addFlashAttribute("secret", newSecret);
+        redirectAttributes.addAttribute("id", form.getId());
+        return "redirect:/" + OAUTH_CLIENTS_URL;
     }
 
 }
