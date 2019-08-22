0f5a15494888ec916fb7850653c0f4e60cc65df6
Bartek Ł <baart92@gmail.com>
2019-08-22 23:30:15 +0200

    Add some more test cases

--- domain-web/jbb-system-web/src/test/java/org/jbb/system/web/logging/controller/AcpLoggingControllerIT.java
+++ domain-web/jbb-system-web/src/test/java/org/jbb/system/web/logging/controller/AcpLoggingControllerIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 the original author or authors.
+ * Copyright (C) 2019 the original author or authors.
  *
  * This file is part of jBB Application Project.
  *
@@ -10,17 +10,8 @@
 
 package org.jbb.system.web.logging.controller;
 
-import static org.jbb.system.web.logging.controller.CommonLoggingConfiguration.correctAppLogger;
-import static org.jbb.system.web.logging.controller.CommonLoggingConfiguration.correctConsoleAppender;
-import static org.jbb.system.web.logging.controller.CommonLoggingConfiguration.correctFileAppender;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 import com.google.common.collect.Lists;
+
 import org.jbb.system.api.logging.LoggingSettingsService;
 import org.jbb.system.api.logging.model.LoggingConfiguration;
 import org.jbb.system.web.BaseIT;
@@ -32,6 +23,18 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.jbb.system.web.logging.controller.CommonLoggingConfiguration.correctAppLogger;
+import static org.jbb.system.web.logging.controller.CommonLoggingConfiguration.correctConsoleAppender;
+import static org.jbb.system.web.logging.controller.CommonLoggingConfiguration.correctFileAppender;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class AcpLoggingControllerIT extends BaseIT {
     @Autowired
@@ -46,6 +49,21 @@ public class AcpLoggingControllerIT extends BaseIT {
     public void setUp() throws Exception {
         mockMvc = MockMvcBuilders.webAppContextSetup(wac)
                 .apply(SecurityMockMvcConfigurers.springSecurity()).build();
+    }
+
+    @Test
+    public void shouldPutLoggingFormToModel_whenGET() throws Exception {
+        // given
+        prepareLoggingConfigurationMocks();
+
+        // when
+        ResultActions result = mockMvc.perform(get("/acp/general/logging"));
+
+        // then
+        result.andExpect(status().isOk())
+                .andExpect(view().name("acp/general/logging"))
+                .andExpect(model().attributeExists("loggingSettingsForm"));
+
     }
 
     @Test
@@ -70,5 +88,6 @@ public class AcpLoggingControllerIT extends BaseIT {
         given(loggingConfigurationMock.getFileAppenders()).willReturn(Lists.newArrayList(correctFileAppender()));
         given(loggingConfigurationMock.getLoggers()).willReturn(Lists.newArrayList(correctAppLogger()));
     }
+
 
 }
